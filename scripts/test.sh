b9c7be07d72f4264f8be420a4633369b6badf20d
Ryan Bernardino <r.bernardino17@gmail.com>
2018-07-24 04:41:49 +0800

    Configures pip to just download the dependencies.
    Note that since we are using the setup.py now, our application source code will also be downloaded into the /build folder.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,9 @@
 # Activate virtual environment
 . /appenv/bin/activate
 
+# Download requirements to the build cache
+pip download -d /build -r requirements_test.txt --no-input
+
 # Install application test requirements
 pip install -r requirements_test.txt
 
