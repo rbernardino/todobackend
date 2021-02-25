ee9e0a0f6bf8cb1cf76ce5a3a2d881d6b89093a8
Ryan Bernardino <r.bernardino17@gmail.com>
2021-02-26 06:56:05 +0800

    Refactors test settings to use MySQL backend

---
+++ src/todobackend/settings/test.py
@@ -0,0 +1,16 @@
+from .base import *
+import os
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+# The convention here is that, say if the 'MYSQL_USER' env variable is not found, then use 'todo' as default
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
+    'USER': os.environ.get('MYSQL_USER','todo'),
+    'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
+    'HOST': os.environ.get('MYSQL_HOST','127.0.0.1'),
+    'PORT': os.environ.get('MYSQL_PORT','3306'),
+  }
+}
